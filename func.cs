7f525a9cad3307b657c68677eb4e14859e84222a
Kitsanapong Warit <kitsanapong.warit@hotmail.com>
2021-11-07 20:23:37 +0700

    file function

---
+++ func.cs
@@ -0,0 +1,121 @@
+using System;
+using System.IO;
+
+namespace func
+{
+
+    public class file_func
+    {
+
+        public static string[] PullData(string file_name)
+        {
+
+            string path = Path(file_name);
+
+            string[] data_base = File.ReadAllLines(path);
+            return data_base;
+
+        }
+
+        public static string Path(string file_name)
+        {
+
+            string path = "./data/" + file_name + ".txt";
+            return path;
+        }
+
+        public static bool ScanData(string check, int column, string[] data_base)
+        {
+
+            column--;
+
+            for (int i = 0; i < data_base.Length; i++)
+            {
+                string[] fields = data_base[i].Split(',');
+
+                if (check == fields[column])
+                {
+                    //Console.WriteLine("fail");
+                    return false;                           //break
+                }
+            }
+
+            //Console.WriteLine("Suc");
+            return true;
+
+        }
+
+    }
+
+
+    public class user_func
+    {
+
+        public static void AddNewUser(string name, string password)
+        {
+            Console.WriteLine(name + "," + password);
+        }
+
+
+        public static bool CheckUserName(string new_user_name)
+        {
+            string file_name = "user";
+            string path = file_func.Path(file_name);
+            string[] user_name = file_func.PullData(file_name);
+
+            return file_func.ScanData(new_user_name, 1, user_name);
+
+        }
+
+        public static string CreateUser()
+        {
+            string new_user_name;
+            int i = 1;
+
+            // user_name condition if(new_user_name) {do something}
+
+
+            do
+            {
+                if (i != 1)
+                {
+                    Console.WriteLine("fail");
+                }
+
+                Console.Write("User : ");
+                new_user_name = Console.ReadLine();
+
+                i++;
+
+            } while (!CheckUserName(new_user_name));
+
+            Console.WriteLine("Suc");
+            return new_user_name;
+        }
+
+        public static string CreatePassWord()
+        {
+
+            string password;
+            string password_confilm;
+            do
+            {
+                Console.Write("Password : ");
+                password = Console.ReadLine();
+
+                Console.Write("Confilm Password : ");
+                password_confilm = Console.ReadLine();
+
+            } while (password != password_confilm);
+
+
+            return password;
+        }
+
+
+
+    }
+
+
+
+}
--- main.cs
+++ main.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using func;
 
 // start
 
@@ -8,88 +9,9 @@ class Program
     static void Main(string[] args)
     {
 
-        AddNewUser(CreateUser(), CreatePassWord());
+        user_func.AddNewUser(user_func.CreateUser(), user_func.CreatePassWord());
 
     } // main
 
-    public static void AddNewUser(string name, string password)
-    {
-        Console.WriteLine(name + "," + password);
-    }
-
-
-    public static bool CheckUserName(string new_user_name)
-    {
-        string file_name = "user";
-        string path = Path(file_name);
-        string[] user_name = PullData(file_name);
-
-        using (StreamWriter file = new StreamWriter(path, true))
-        { // start new_user_name
-
-            for (int i = 0; i <= (user_name.Length - 1); i++)
-            {
-                string[] fields = user_name[i].Split(',');
-
-                if (new_user_name == fields[0])
-                {
-                    Console.WriteLine("fail");
-                    return false;
-                }
-            }
-
-            Console.WriteLine("Suc");
-            return true;
-
-        } // end new_user_name
-
-    }
-
-    public static string CreateUser()
-    {
-        string new_user_name;
-        do
-        {
-            Console.Write("User : ");
-            new_user_name = Console.ReadLine();
-        }
-
-        while (!CheckUserName(new_user_name));
-        return new_user_name;
-    }
-
-    public static string CreatePassWord()
-    {
-
-        string password;
-        string password_confilm;
-        do
-        {
-            Console.Write("Password : ");
-            password = Console.ReadLine();
-
-            Console.Write("Confilm Password : ");
-            password_confilm = Console.ReadLine();
-
-        } while (password != password_confilm);
-
-
-        return password;
-    }
-
-    public static string[] PullData(string file_name){
-    
-        string path = Path(file_name);
-
-        string[] data_base = File.ReadAllLines(path);
-        return data_base;
-
-    }
-
-    public static string Path(string file_name){
-
-        string path = "./data/" + file_name + ".txt";
-        return path;
-    }
 
 }
