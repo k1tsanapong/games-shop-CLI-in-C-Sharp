d88906ed25972c4989ff9de6dac703a119b93955
Kitsanapong Warit <kitsanapong.warit@hotmail.com>
2021-11-07 23:19:09 +0700

    sace

--- select.cs
+++ select.cs
@@ -18,7 +18,8 @@ namespace SelectMenu
 
         }
 
-        public static void login(){
+        public static void login()
+        {
 
             Console.Clear();
 
@@ -31,11 +32,42 @@ namespace SelectMenu
 
         }
 
-        public static void signup(){
+        public static void signup()
+        {
 
             user_func.AddNewUser(user_func.CreateUser(), user_func.CreatePassWord());
-            
+
         }
     }   // end class main_menu
+
+    public class menu 
+    {   
+        public static void selection()
+        {
+                Console.WriteLine("*----Menu----*");
+                Console.WriteLine("1.Shop");
+                Console.WriteLine("2.Libray");
+                Console.WriteLine("3.Return");
+                Console.Write("Select ....");
+        }
+
+        public static void shop()
+        {
+                    Console.WriteLine("*----Shop----*");
+                    Console.WriteLine("1. gta x");
+                    Console.WriteLine("2.cyberpunk 1999");
+                    Console.WriteLine("3.old world");
+                    Console.WriteLine("4.mario cart");
+                    Console.WriteLine("5.return");
+        }
+
+        public static void libary()
+        {
+                    Console.WriteLine("*----Libary----*");
+
+        }
+
+    }
+
 
 }
