ec054d7825f6c0f8eb32f9ff29ba1f0fcbe12752
Diavolo01 <6431503042@lamduan.mfu.ac.th>
2021-11-14 22:23:59 +0700

    Topup

--- main.cs
+++ main.cs
@@ -9,72 +9,147 @@ class Program
     static void Main(string[] args)
     {
         bool success;   // use for no error input
-        int select, userInput = 1;
+        int select, userInput = 1, balance = 500;
         string user_name = "";
-        
-        main_menu.selection();
 
+        string[] game_name = { };
+        string[] game_pirce = { };
+
+        Console.WriteLine("Game library");
+
+        // for loop start
+
+        Console.WriteLine("1.GTA X({0} Baht.");
+        Console.WriteLine("2.cyberpunk 1999({0} Baht.");
+        Console.WriteLine("3.old world({0} Baht.");
+        Console.WriteLine("4.mario cart({0} Baht.");
+
+        // for loop end
+
+
+        // for loop end
+
+        // show ตังค์  สักเลข สมุมติ 5.      Console.WriteLine(balance);
+        // 1.เติมตังค์
+        // 0. return
+        // show return 0.
+
+
+        Console.WriteLine("select = ");
         success = Int32.TryParse(Console.ReadLine(), out select);   // input select
+
+
+        // ---------shop----------------//
+
         userInput = 1;
         while (userInput != 0)
         {
             switch (select)
             {
-
                 case 1:
-                    userInput--;
-                    user_name = main_menu.login();
+                    userInput--;    // userInput = 0  exit loop
+                    // 
                     break;
 
-                case 2:
+
+
+
+
+
+
+                // ---Your Wallet--- //
+
+                case 5:
                     userInput--;
-                    main_menu.signup();
+                    Console.WriteLine("----Your Wallet----");
+                    Console.WriteLine($"Current Wallet balance = {0} Baht");//return wallet
+                    Console.WriteLine("1.Topup");
+                    Console.WriteLine("0.return");
+                    success = Int32.TryParse(Console.ReadLine(), out select);   // input select
+                    userInput = 1;
+                    while (userInput != 0)
+                    {
+                         switch(select)
+                    {
+                            case 1:
+                             userInput--;
+                             Console.WriteLine($"Steam wallet code : {0}");//return wallet
+                            break;
+                            case 0:
+                               break;
+                        // ฝาก 0.returnด้วย
+                            default:
+                             Console.WriteLine("Please select only 1 or 0");
+                               break;
+                    }
+
+                    }       // while end
+
+                    
+
+
+
+                   break;   // case 5 end
+
+                // ---Your Wallet--- //
+
+                default:
+                    break;          // exit loop
+            }
+
+
+
+        }
+
+
+        // ---------shop----------------//
+
+
+        Console.WriteLine("-----GTA X-----------)");
+        // show  ราคา
+        // show ตังค์
+        Console.WriteLine("Comfirm purchase?");
+        Console.WriteLine("1.yes");
+        // 
+
+
+        Console.WriteLine("2.no");
+
+
+        // Comfirm Start
+
+        userInput = 1;
+        while (userInput != 0)
+        {
+            switch (select)
+            {
+                case 1:
+                    userInput--;    // userInput = 0  exit loop
+
+                    // if (balance > game_pirce[i])
+                    // {
+
+                    //     balance = balance - game_pirce[i];
+
+                    // // Add game to your libary; KIM ทำ
+
+                    //     else
+                    //     {
+                    //         Console.WriteLine("Fuck you");
+                    //     }
+                    // }
+
                     break;
 
                 default:
-                    Console.WriteLine("Do again");
-                    success = Int32.TryParse(Console.ReadLine(), out select);   // input select
-                    break;
+                    break;          // exit loop
             }
+
+
+
         }
 
-
-
-        menu.selection();
-        success = Int32.TryParse(Console.ReadLine(), out select);   // input select
-
-        switch (select)
-        {
-            case 1:
-                menu.shop();
-                break;
-            case 2:
-                menu.libary(user_name);
-                break;
-            case 3:
-                ///return>>. ก่อนหน้า
-                break;
-            default:
-                Console.WriteLine("Please select 1-3");
-                break;
-        }
-
-
-        ///รีเทิร์นฝากเหล่าเทพเจ้าช่วยด้วย
-        success = Int32.TryParse(Console.ReadLine(), out select);   // input select
-        switch (select)
-        {
-            case 1:
-                Console.WriteLine("yes");
-                break;
-            case 2:
-                Console.WriteLine("no");
-                break;
-            default:
-                Console.WriteLine("Select ONLY 1,2");
-                break;
-        }
-
+        // Comfirm End
 
     } // main
 
